bd922bedfe2236d9dae569f520d8fb85c8a4da4f
Julien Juret <julien.juret@gmail.com>
2024-06-30 17:17:53 +0200

    Simplify entrypoint
    Signed-off-by: Julien Juret <julien.juret@gmail.com>

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/bootstrap.php';
+require __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . '/src/exceptions/ErrorHandler.php';
+
+set_error_handler(['exceptions\ErrorHandler', 'handleError']);
+set_exception_handler(['exceptions\ErrorHandler', 'handleException']);
+
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+header("content-type: application/json; charset=UTF-8");
 
 require __DIR__ . '/src/routes/api.php';
